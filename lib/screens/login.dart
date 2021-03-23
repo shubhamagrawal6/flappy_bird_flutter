a6ef7613392882037ef485268c731d81647266a8
shubhamagrawal6 <shubhamagrawal322@gmail.com>
2021-03-24 01:43:08 +0530

    splash imlemented and firebase connected

---
+++ lib/screens/login.dart
@@ -0,0 +1,24 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class Login extends StatefulWidget {
+  final FirebaseAuth auth;
+  final FirebaseFirestore firestore;
+
+  const Login({
+    Key key,
+    @required this.auth,
+    @required this.firestore,
+  }) : super(key: key);
+
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
